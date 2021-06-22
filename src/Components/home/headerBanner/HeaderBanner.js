f790da514bdebaa8760fda5546a995177f6a9c46
Jobayer-Majumder <jobayermojumder70@gmail.com>
2021-06-23 00:14:07 +0600

    fixed deploy problem

--- src/Components/home/headerBanner/HeaderBanner.js
+++ src/Components/home/headerBanner/HeaderBanner.js
@@ -1,8 +1,7 @@
 import React from 'react';
-import profile from '../../../images/undraw_young_and_happy_hfpe.svg';
+import profile from '../../../images/hire-dedicated-reactjs-developer.png';
 import Typewriter from 'typewriter-effect';
 import Fade from 'react-reveal/Fade';
-import Zoom from 'react-reveal/Zoom';
 import { Link } from 'react-scroll';
 
 
@@ -10,10 +9,9 @@ import { Link } from 'react-scroll';
 const HeaderBanner = () => {
     return (
         <header className='bg-dark d-flex align-items-center' style={{ height: '100vh' }}>
-
             <div className="container">
                 <div className="d-flex flex-column flex-column-reverse flex-md-row py-4">
-                    <Zoom>
+                    <Fade bottom>
                         <div className="col-md-6 d-flex align-items-center justify-content-center mb-3">
                             <div className='text-center text-md-start py-4'>
                                 <h1 className='text-white'>HELLO! <br /> <span className='text-brand-lite'>I'M JOBAYER MAJUMDER</span> </h1>
@@ -33,12 +31,12 @@ const HeaderBanner = () => {
                                 <a className="btn btn-brand text-secondary rounded-pill" href="https://drive.google.com/uc?export=download&id=1E5d9j1eSjTPXzivJx5ZXxkaJm_7zQlY5" role="button" rel="noreferrer">Download Resume</a>
                             </div>
                         </div>
-                    </Zoom>
-                    <Zoom>
+                    </Fade>
+                    <Fade bottom>
                         <div className="col-md-6 text-center">
                             <img src={profile} className='img-fluid' alt="" />
                         </div>
-                    </Zoom>
+                    </Fade>
                 </div>
             </div>
         </header>
